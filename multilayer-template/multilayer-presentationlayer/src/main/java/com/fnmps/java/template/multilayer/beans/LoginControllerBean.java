73dbcb02b2385bc93fddbe3e24a1f35b4ae55a7e
fnmps <fnmps@PC-fnmps>
2017-10-29 20:35:23 +0000

    Added service layer and business layer (unfinished)

--- multilayer-template/multilayer-presentationlayer/src/main/java/com/fnmps/java/template/multilayer/beans/LoginControllerBean.java
+++ multilayer-template/multilayer-presentationlayer/src/main/java/com/fnmps/java/template/multilayer/beans/LoginControllerBean.java
@@ -7,6 +7,10 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.fnmps.java.template.multilayer.servicelayer.authentication.exceptions.AuthenticationException;
+import com.fnmps.java.template.multilayer.servicelayer.authentication.model.UserIn;
+import com.fnmps.java.template.multilayer.servicelayer.authentication.services.IAuthenticationService;
+
 @Named
 @ViewScoped
 public class LoginControllerBean implements Serializable {
@@ -23,16 +27,24 @@ public class LoginControllerBean implements Serializable {
 	private LoginBackingBean loginBackingBean;
 
 	@Inject
-	private ServiceBean serviceBean;
+	private IAuthenticationService authenticationService;
 
 	@PostConstruct
 	public void init() {
 		System.out.println("initing services");
 	}
 
-	public String actionLogin() {
-		userBean.setUsername(loginBackingBean.getUsername());
-
+	public String actionLogin() {	
+		
+		try {
+			UserIn user = new UserIn();
+			user.setUsername(loginBackingBean.getUsername());
+			authenticationService.authenticate(user );
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}
+		
+		userBean.setUsername(loginBackingBean.getUsername());		
 		return "userRelationships.xhtml";
 	}
 
@@ -51,13 +63,13 @@ public class LoginControllerBean implements Serializable {
 	public void setLoginBackingBean(LoginBackingBean loginBackingBean) {
 		this.loginBackingBean = loginBackingBean;
 	}
-	
-	public ServiceBean getServiceBean() {
-		return serviceBean;
+
+	public IAuthenticationService getAuthenticationService() {
+		return authenticationService;
 	}
 
-	public void setServiceBean(ServiceBean serviceBean) {
-		this.serviceBean = serviceBean;
+	public void setAuthenticationService(IAuthenticationService authenticationService) {
+		this.authenticationService = authenticationService;
 	}
 
 
